00444ddfdb99ba2a312631d311f1488a5634867d
Wasith Theerapattrathamrong <phai@live.com>
2019-11-14 23:33:40 +0700

    New line to keep it 120

--- ios-ui-test-specify-start-view-controller/AppDelegate.swift
+++ ios-ui-test-specify-start-view-controller/AppDelegate.swift
@@ -5,7 +5,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication,
+                     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         if let viewController = startFromStoryboard() {
             window = UIWindow(frame: UIScreen.main.bounds)
             window?.rootViewController = viewController
