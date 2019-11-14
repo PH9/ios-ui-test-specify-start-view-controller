c6e2836ab9d9807f96b08592f6c3c7536b37564d
Wasith Theerapattrathamrong <phai@live.com>
2019-11-14 22:50:58 +0700

    init

---
+++ ios-ui-test-specify-start-view-controller/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ios-ui-test-specify-start-view-controller
+//
+//  Created by Wasith Theerapattrathamrong on 14/11/2562 BE.
+//  Copyright © 2562 Wasith Theerapattrathamrong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
