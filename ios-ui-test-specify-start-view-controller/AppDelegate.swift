df7f21d8e0ec22592ad30b373c789b6ed7b1d857
Wasith Theerapattrathamrong <phai@live.com>
2019-11-14 23:19:57 +0700

    Added way to start with specify storyboard

--- ios-ui-test-specify-start-view-controller/AppDelegate.swift
+++ ios-ui-test-specify-start-view-controller/AppDelegate.swift
@@ -6,7 +6,25 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        if let viewController = startFromStoryboard() {
+            window = UIWindow(frame: UIScreen.main.bounds)
+            window?.rootViewController = viewController
+            window?.makeKeyAndVisible()
+        }
+
         return true
+    }
+
+    private func startFromStoryboard(bundle: Bundle? = nil) -> UIViewController? {
+        guard let name = ProcessInfo.processInfo.environment["startWithStoryboardName"] else {
+            return nil
+        }
+
+        guard let viewController = UIStoryboard(name: name, bundle: bundle).instantiateInitialViewController() else {
+            return nil
+        }
+
+        return viewController
     }
 }
 
